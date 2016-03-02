8d19e8ac125020729ae442877baf848f9420f732
Jill Donohue <jillmd501@gmail.com>
2016-03-02 15:05:36 -0700

    can create poll on steve's page

--- server.js
+++ server.js
@@ -11,14 +11,56 @@ const server = http.createServer(app)
 const socketIo = require('socket.io');
 const io = socketIo(server);
 
+const bodyParser = require('body-parser');
+const countVotes = require('./lib/count-votes');
+const votes = require('./lib/votes');
+const generateId = require('./lib/generate-id');
+
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
 app.locals.title = 'Steve Time';
+app.locals.polls = {};
+
+//routes
 
 app.get('/', function (req, res){
   res.sendFile(__dirname + '/public/index.html');
 });
 
-var votes = {};
+app.post('/poll', function(req, res){
+  var poll = req.body.poll;
+  var id = generateId();
+  var adminId = req.body.poll.adminId;
+  app.locals.polls[id] = poll;
+  poll['adminId'] = adminId;
+  poll['id'] = id;
+  poll['votes'] = [];
+  res.redirect('/polls/' + id + "/" + adminId);
+});
+
+app.get('/polls/:id', function(req, res){
+  var poll = app.locals.polls[req.params.id];
+  res.render('admin-poll-view', {poll: poll, votes: countVotes(poll)});
+});
+
+app.get('/polls/admin/:adminId', function(req, res){
+  var pollList = [];
+  var keys = Object.keys(app.locals.polls)
+  for (var i = 0; i < keys.length; i++){
+    var poll = app.locals.polls[keys[i]];
+    if(poll['adminId'] === req.params.adminId){ pollList.push(poll)}
+  }
+  res.render('steve-admin-view', {polls: pollList});
+})
+
+app.get('/polls/:id/:adminId', function(req, res){
+  var poll = app.locals.polls[req.params.id];
+  res.render('steve-admin-view', {poll: poll, id: req.params.id, adminID: req.params.adminId, votes: countVotes(poll)});
+})
+
+//sockets
 
 io.on('connection', function (socket) {
   console.log('A user has connected.', io.engine.clientsCount);
@@ -42,19 +84,5 @@ io.on('connection', function (socket) {
 	  io.sockets.emit('usersConnected', io.engine.clientsCount);
 	});
 });
-
-
-function countVotes(votes) {
-var voteCount = {
-    'This is old hat to me': 0,
-    'I have an okay understanding of this': 0,
-    'I have no idea what you are babbling about': 0,
-    'Where am I?': 0
-};
-  for (var vote in votes) {
-    voteCount[votes[vote]]++
-  }
-  return voteCount;
-}
 
 module.exports = app;
